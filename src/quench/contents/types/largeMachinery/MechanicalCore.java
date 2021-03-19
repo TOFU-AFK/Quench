9b0b3dc0cb1639176e870b395313c6cdba6c299a
TOFU-AFK <2381855250@qq.com>
2021-03-19 19:02:45 +0800

    修复-电力出口

--- src/quench/contents/types/largeMachinery/MechanicalCore.java
+++ src/quench/contents/types/largeMachinery/MechanicalCore.java
@@ -199,7 +199,7 @@ public class MechanicalCore extends LargeMachinery{
             for(BlockData data:structure.datas){
                 if(structure.datas.length>mechanicalData.getTiles().size()){
                 //先获取地图的tile，再将build强制转换成LargeMachineryBuild，因为前面已经判断了方块的名称，所以不用担心出现问题
-                int r;//如果方块可旋转，则保留旋转值
+                int r = 0;//如果方块可旋转，则保留旋转值
                 Tile tile = Vars.world.tile((int) tile().x+data.x(direction)/8,(int) tile().y+data.y(direction)/8);
                 if(tile.block().rotate){
                   r = tile.build.rotation;
