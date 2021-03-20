f0fbb490b05dfcc4eaf5ea37977701ebdd9107f4
TOFU-AFK <2381855250@qq.com>
2021-03-20 18:14:12 +0800

    测试-动力传输

--- src/quench/contents/drawers/largeMachinery/DrawMotiveTransmission.java
+++ src/quench/contents/drawers/largeMachinery/DrawMotiveTransmission.java
@@ -61,7 +61,7 @@ public class DrawMotiveTransmission extends DrawLargeMachinery {
 	public void draw(LargeMachineryBuild entity){
 	    time++;
 	    //定时将索引加一
-	    if(time>=28){
+	    if(time>=4){
 	      time = 0;
 	     if(index+1>=quantity){
 	       index = 0;
