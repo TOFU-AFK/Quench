b8b2e115556eda7454313ebd90e5062e90905c2b
TOFU-AFK <2381855250@qq.com>
2021-03-27 16:29:04 +0800

    多方块炮塔

--- src/quench/contents/blocks/QULargeMachinery.java
+++ src/quench/contents/blocks/QULargeMachinery.java
@@ -81,11 +81,13 @@ public class QULargeMachinery implements ContentList
 		universalBase = new TurretCore("universalBase")
 				{
 					{
-		      requirements(Category.power, with(Items.silicon, 90, Items.lead, 85,Items.titanium, 50));
+		      requirements(Category.turret, with(Items.silicon, 90, Items.lead, 85,Items.titanium, 50));
 					size = 1;
 					health = 40*size*size;
 					efficiency = 1.2f;
-					structure = new Structure(new BlockData[]{new BlockData(basicBlock,16,16),new BlockData(basicBlock,8,16),new BlockData(basicBlock,-8,16),new BlockData(basicBlock,-16,16),new BlockData(basicBlock,16,8),new BlockData(basicBlock,8,8),new BlockData(basicBlock,-8,8),new BlockData(basicBlock,-16,8),new BlockData(basicBlock,16,-8),new BlockData(basicBlock,8,-8),new BlockData(basicBlock,-8,-8),new BlockData(basicBlock,-16,-8),new BlockData(basicBlock,16,-16),new BlockData(basicBlock,8,-16),new BlockData(basicBlock,-8,-16),new BlockData(basicBlock,-16,-16)});//一方块8像素
+					structure = new Structure(new BlockData[]{new BlockData(basicBlock,16,16),new BlockData(basicBlock,8,16),new BlockData(basicBlock,-8,16),new BlockData(basicBlock,-16,16),new BlockData(basicBlock,16,8),new BlockData(basicBlock,8,8),new BlockData(basicBlock,-8,8),new BlockData(basicBlock,-16,8),new BlockData(basicBlock,16,-8),new BlockData(basicBlock,8,-8),new BlockData(basicBlock,-8,-8),new BlockData(basicBlock,-16,-8),new BlockData(basicBlock,16,-16),new BlockData(basicBlock,8,-16),new BlockData(basicBlock,-8,-16),new BlockData(basicBlock,-16,-16),new BlockData(basicBlock,16,0),new BlockData(basicBlock,8,0),new BlockData(basicBlock,-8,0),new BlockData(basicBlock,-16,0),
+					  new BlockData(basicBlock,0,16),new BlockData(basicBlock,0,0),new BlockData(basicBlock,0,-8),new BlockData(basicBlock,0,-16)
+					});//一方块8像素
 					//添加科技树
 					new TechNode(TechTree.get(hydroelectricGeneratorCore), this,new ItemStack[]{new ItemStack(Items.titanium, 2)});
 					}
