8fb2c71b33306d215f3aacd1384c757270d6e206
TOFU-AFK <2381855250@qq.com>
2021-04-03 11:34:53 +0800

    多方块炮塔

--- src/quench/contents/types/largeMachinery/turret/LargeTurret.java
+++ src/quench/contents/types/largeMachinery/turret/LargeTurret.java
@@ -226,7 +226,7 @@ public class LargeTurret{
           peekAmmo().create(core,core.team(),core.x,core.y,rotation);
         }else{
           for(int i=0;i<shots;i++){
-            peekAmmo().create(core,core.team(),core.x,core.y,rotation+Mathf(0,bulletOffset));
+            peekAmmo().create(core,core.team(),core.x,core.y,rotation+Mathf.random(0,bulletOffset));
           }
         }
       }
