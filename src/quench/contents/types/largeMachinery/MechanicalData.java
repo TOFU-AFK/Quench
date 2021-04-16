445aa6deda3eca7a6748c40f7e81bd1a44968372
TOFU-AFK <2381855250@qq.com>
2021-04-16 19:19:52 +0800

    多方块炮塔

--- src/quench/contents/types/largeMachinery/MechanicalData.java
+++ src/quench/contents/types/largeMachinery/MechanicalData.java
@@ -123,15 +123,15 @@ public class MechanicalData{
     public int getRotation(){
       switch (core.direction){
             case 0:
-                return 2;
+                return 1;
             case 1:
-                return 1;
+                return 2;
             case 2:
-                return 1;
+                return 3;
             case 3:
                 return 0;
               }
-              return 2;
+              return 1;
     }
     
     public void update(){
