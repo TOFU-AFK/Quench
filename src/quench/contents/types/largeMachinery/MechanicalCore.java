bb37a879c522ae94d53cf3f3f946a9f5c40cbb89
TOFU-AFK <2381855250@qq.com>
2021-04-17 20:36:13 +0800

    火力引擎

--- src/quench/contents/types/largeMachinery/MechanicalCore.java
+++ src/quench/contents/types/largeMachinery/MechanicalCore.java
@@ -67,7 +67,6 @@ public class MechanicalCore extends LargeMachinery{
         super(name);
         solid = true;
         destructible = true;
-        //group = BlockGroup.none;
         configurable = true;
     }
     
