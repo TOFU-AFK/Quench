fc6110b3586d4f56ef08e0f93f3050d30529657b
TOFU-AFK <2381855250@qq.com>
2021-04-26 16:24:07 +0800

    修复动力源

--- src/quench/contents/types/largeMachinery/draw/DrawLargeMachinery.java
+++ src/quench/contents/types/largeMachinery/draw/DrawLargeMachinery.java
@@ -12,7 +12,7 @@ public class DrawLargeMachinery{
     public boolean isLoader = false;
     public void draw(LargeMachineryBuild entity){
         //Draw.rect(bottom,x,y);
-        Draw.rect(entity.block.region,entity.x,entity.y,entity.rotation);
+        Draw.rect(entity.block.region,entity.x,entity.y,entity.rotation*90);
     }
 
     public void load(Block block){
